dff16a1758c75b137e3853d3151f524b8e6afefd
tongqinyuan <yuantongqin@ccmtjf.com>
2018-03-23 14:39:50 +0800

    删除重新添加1.0

--- ReactAndroidBridge/reduxa/actions/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by tongqinyuan on 2018/3/22.
- */
-
-/*
- 用于创建Action 与一些公用常量
- */
--- ReactAndroidBridge/reduxa/reducers/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by tongqinyuan on 2018/3/22.
- */
-
-/*
-    用于合并成一个state的reduces方法
- */
